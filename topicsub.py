564d4a189c0b5a65698ca1ff049ec7e2ab624452
jiyoony <cjy3378@kookmin.ac.kr>
2021-09-24 10:42:04 +0900

    토픽 별로 수신

--- topicsub.py
+++ topicsub.py
@@ -14,6 +14,14 @@ def on_connect(client,userdata,flag,rc):
   # print("client",client)
   # print("flag= ",flag)
 
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+ 
+
 filenum = {}
 def on_message(client,userdata,message):
   message = str(message.payload.decode("utf-8"))
@@ -22,33 +30,37 @@ def on_message(client,userdata,message):
   
   # f = null
    #파일이 몇번째 줄까지 써졌는지 확인
-  if (len(meslist) == 3):
+  if (len(meslist) == 4):
     print("meslist = ", meslist)
     # print("meslist[1] = ", meslist[1])
     print("filenum = ", filenum)
     # print("filenum[meslist[0]] = ", filenum[meslist[0]])
     print("-------------")
-    if os.path.isfile('/home/kmuscrc/Desktop/MDS/'+meslist[0]): #파일이 존재할 경우
+    createFolder('/home/kmuscrc/Desktop/MDS/'+meslist[0])
+    if os.path.isfile('/home/kmuscrc/Desktop/MDS/'+meslist[0]+meslist[1]): #파일이 존재할 경우
         print("YES FILE")
-        with open('/home/kmuscrc/Desktop/MDS/'+meslist[0]) as myfile:
+        with open('/home/kmuscrc/Desktop/MDS/'+meslist[0]+meslist[1]) as myfile:
             total_lines = sum(1 for line in myfile)
         print(total_lines)
-        filenum[meslist[0]] = total_lines + 1 #원하는 건 다음번 줄 
+        filenum[meslist[1]] = total_lines + 1 #원하는 건 다음번 줄 
         print(filenum.keys())
         print(filenum.values())
     else:
-      print('/home/kmuscrc/Desktop/MDS/'+meslist[0])
+      print('/home/kmuscrc/Desktop/MDS/'+meslist[0]+meslist[1])
       print("NO FILE")
-      filenum[meslist[0]] = 1
+      filenum[meslist[1]] = 1
       print(filenum)
 
-    if (int(meslist[1]) == filenum[meslist[0]]): #행수가 같으면
-      print("in",meslist[1])
-      f = open(meslist[0], 'a')
-      f.write(meslist[2])
-      filenum[meslist[0]] += 1
-      f.close()
-      print(filenum[meslist[0]])
+    if (int(meslist[2]) == filenum[meslist[1]]): #행수가 같으면
+      if (meslist[3] != 'done'):
+        print("in",meslist[2])
+        f = open(meslist[1], 'a')
+        f.write(meslist[3])
+        filenum[meslist[1]] += 1
+        f.close()
+        print(filenum[meslist[1]])
+      else:
+        client1.publish(meslist[0],meslist[1]+":delete")
     else :
       print("i dont want this message")
   else:
@@ -64,7 +76,7 @@ broker_address="192.168.50.240"
 client1 = mqtt.Client("client1") #구독자 이름
 client1.on_connect = on_connect
 client1.connect(broker_address) #서버에 접속
-client1.subscribe("outTopic") #구독하는 토픽이름
+client1.subscribe("BIOLOGGER/#") #구독하는 토픽이름
 client1.on_message = on_message #callback 함수로 등록
 client1.loop_forever() #무한반복  
 
