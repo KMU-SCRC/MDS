93e1382edd65a93cece74caf19b64c0c0e88c98d
kmuscrc <cjy3378@kookmin.ac.kr>
2021-10-14 19:28:39 +0900

    핫스팟 설정 SH

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+sudo iw phy phy0 interface add wlan1 type managed
+sudo nmcli con up Hostspot
 sudo kill $(sudo lsof -t -i:1883)
 khadas
 mosquitto &
 cd /home/kmuscrc/Desktop/MDS
 mkdir imin
-python3 mqttnewsub.py 
+python3 topicsubone.py 
